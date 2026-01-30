1a2895cce7f3ef1c122f04ec611b950feb770f17
AnLe <anle.noctis@gmail.com>
2026-01-30 11:14:24 +0700

    config css resource file

--- src/main/java/com/example/laptopshop/config/WebMvcConfig.java
+++ src/main/java/com/example/laptopshop/config/WebMvcConfig.java
@@ -4,10 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import ch.qos.logback.core.model.processor.ResourceHandlerBase;
 
 @Configuration
 @EnableWebMvc
@@ -25,5 +28,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.viewResolver(viewResolver());
+    }
+
+    // Config css
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/resources/css");
     }
 }
