314ab450c0e76af0ffe281c016cd891350aacd9a
AnLe <anle.noctis@gmail.com>
2026-02-03 10:26:45 +0700

    Admin Layout

--- src/main/java/com/example/laptopshop/LaptopshopApplication.java
+++ src/main/java/com/example/laptopshop/LaptopshopApplication.java
@@ -1,9 +1,14 @@
 package com.example.laptopshop;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		SecurityAutoConfiguration.class,
+		ManagementWebSecurityAutoConfiguration.class
+})
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
